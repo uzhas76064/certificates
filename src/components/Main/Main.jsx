61d33b06ecf8f282a6ff6298492f6847c2858436
Maksim <uzhas76064@gmail.com>
2024-11-22 12:14:42 +0300

    Добавил Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import styles from './Main.module.css'
+
+const Main = ({children}) => {
+    return (
+        <main className={styles.main}>
+            {children}
+        </main>
+    )
+}
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from "./components/Header/Header.jsx";
+import Main from "./components/Main/Main.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.querySelector('body')).render(
   <StrictMode>
       <Header title='Магазин сертификатов'/>
+      <Main>
+
+      </Main>
   </StrictMode>,
 )
