31415578bbf8d965296c6b872e1236205e265a26
Maksim <uzhas76064@gmail.com>
2024-11-22 12:26:26 +0300

    Добавил footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import styles  from './Footer.module.css';
+
+const Footer = ({title, year}) => (
+    <footer className={styles.footer}>
+        <p className={styles.footer__text}>&copy; {year} {title}</p>
+    </footer>
+)
+
+Footer.propTypes = {
+    title: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
+import Cards from "./components/Cards/Cards.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 createRoot(document.querySelector('body')).render(
   <StrictMode>
       <Header title='Магазин сертификатов'/>
       <Main>
-
+          <Cards/>
       </Main>
+      <Footer year='2024' title='Магазин сертификатов'/>
   </StrictMode>,
 )
